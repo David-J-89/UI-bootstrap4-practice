8156cd4c0b9f90fed33b8009063fbd5bcb78ff71
David Whaley <davidwhaleycode@outlook.com>
2019-01-16 21:51:25 -0600

    first proj

---
+++ angular-bootstrap-example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.']
+})
+export class AppComponent {
+  title = 'abe';
+}
